96f905c1f243b3e746757df475d3c30c78be609e
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-04-19 18:57:00 -0700

    Experimenting with Sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Header from "./Header.svelte";
+</script>
+
+<div class="app">
+    <Header />
+
+    <main>
+        <slot />
+    </main>
+
+    <footer>
+        <p>And thats all folks!</p>
+    </footer>
+</div>
+
+<style>
+    footer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
